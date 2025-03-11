6af7bf07afb8c505596e91fa4dce82f532945282
Peter <skwortsow.petia@yandex.ru>
2025-03-11 12:02:54 +0300

    Анимированная галактика

--- main.js
+++ main.js
@@ -1,88 +1,184 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import g, { GUI } from 'three/addons/libs/lil-gui.module.min.js'
-
-import fragmentShader from './shaders/fragment.js';
-import vertexShader from './shaders/vertex.js';
-
-const gui = new GUI()
-const debugObject = {}
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js'
+import vertex from './shaders/vertex';
+import fragment from './shaders/fragment'
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setAnimationLoop(animate);
-document.body.appendChild(renderer.domElement);
-
-const geometry = new THREE.PlaneGeometry(5, 5, 500, 500);
-
-debugObject.depthColor = '#186691';
-debugObject.surfaceColor = '#9bd8ff';
+const gui = new GUI()
 
 
+const asperctRatio = innerWidth / innerHeight
+const textureLoader = new THREE.TextureLoader()
 
-const material = new THREE.ShaderMaterial({
-    vertexShader: vertexShader,
-    fragmentShader: fragmentShader,
-    side: THREE.DoubleSide,
-    uniforms: {
-        uTime: { value: 0 },
+// галактика
 
-        uBigWavesElevation: { value: 0.2 },
-        uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5) }, // вектор 2 означает направление по - x, y
-        uBigWavesSpeed: { value: 0.5 },
+const parametrs = {
+    count: 10000,
+    size: 0.02,
+    radius: 5,
+    branches: 3,
+    spin: 1,
+    randomness: 0.2,
+    pandomnessPower: 3,
+    insideColor: '#ff6030',
+    outsideColor: '#1b3984'
+}
 
-        uSmallWavesElevation: { value: 0.15 },
-        uSmallWavesFrequency: { value: 3.0 },
-        uSmallWavesSpeed: { value: 0.2 },
-        uSmallWavesIterations: { value: 3.0 },
+let geometry, material, points = null
 
+const generatyGalaxy = () => {
 
-        udDephColor: { value: new THREE.Color(debugObject.depthColor) },
-        uSurfaceColor: { value: new THREE.Color(debugObject.surfaceColor) },
-        uColorOffset: { value: 0.08 },
-        uColorMultiplier: { value: 5.0 },
-
-
+    if (points !== null) {
+        geometry.dispose()
+        material.dispose()
+        scene.remove(points)
     }
- });
-const mesh = new THREE.Mesh(geometry, material);
-mesh.rotation.x = Math.PI * 0.5;
-scene.add(mesh);
 
-camera.position.z = 3;
-camera.position.y = 3;
-const controls = new OrbitControls(camera, renderer.domElement);
+    geometry = new THREE.BufferGeometry()
+    const positions = new Float32Array(parametrs.count * 3)
 
-gui.add(mesh.material.uniforms.uBigWavesElevation, 'value').min(0).max(2).step(0.01).name('высота волны');
-gui.add(mesh.material.uniforms.uBigWavesFrequency.value, 'x').min(0).max(10).step(0.01).name('частота волны по x');
-gui.add(mesh.material.uniforms.uBigWavesFrequency.value, 'y').min(0).max(10).step(0.01).name('частота волны по y');
-gui.add(mesh.material.uniforms.uBigWavesSpeed, 'value').min(0).max(5).step(0.01).name('скорость волны');
+    const colors = new Float32Array(parametrs.count * 3)
+    const scales = new Float32Array(parametrs.count * 1)
+    const randomness = new Float32Array(parametrs.count * 3)
 
 
-gui.addColor(debugObject, 'depthColor').name('цвет глубины').onChange(() => {
-    mesh.material.uniforms.udDephColor.value.set(debugObject.depthColor);
-})
-gui.addColor(debugObject, 'surfaceColor').name('цвет поверхности').onChange(() => {
-    mesh.material.uniforms.uSurfaceColor.value.set(debugObject.surfaceColor);
-})
-gui.add(mesh.material.uniforms.uColorOffset, 'value').min(0).max(5).step(0.01).name('смещение цвета');
-gui.add(mesh.material.uniforms.uColorMultiplier, 'value').min(0).max(10).step(0.01).name('множитель цвета');
-gui.add(mesh.material.uniforms.uSmallWavesElevation, 'value').min(0).max(2).step(0.01).name('высота волны');
-gui.add(mesh.material.uniforms.uSmallWavesFrequency, 'value').min(0).max(30).step(0.01).name('частота волны');
-gui.add(mesh.material.uniforms.uSmallWavesSpeed, 'value').min(0).max(5).step(0.01).name('скорость волны');
-gui.add(mesh.material.uniforms.uSmallWavesIterations, 'value').min(0).max(5).step(1.0).name('количество волны');
+    const colorInside = new THREE.Color(parametrs.insideColor)
+    const colorOutside = new THREE.Color(parametrs.outsideColor)
+    colorInside.lerp(colorOutside, 0.5)
 
 
-const clock = new THREE.Clock();
-function animate() {
+    for (let i = 0; i < parametrs.count; i++) {
 
-    const elapsedTime = clock.getElapsedTime();
+        const radius = Math.random() * parametrs.radius
+        const spinAngle = radius * parametrs.spin
+        const branchesAngle = (i % parametrs.branches) / parametrs.branches * Math.PI * 2
 
-    mesh.material.uniforms.uTime.value = elapsedTime;
-    controls.update();
-    renderer.render(scene, camera);
+      
+
+        const i3 = i * 3
+        positions[i3] = Math.cos(branchesAngle + spinAngle) * radius;
+        positions[i3 + 1] = 0;
+        positions[i3 + 2] = Math.sin(branchesAngle + spinAngle) * radius;
+
+
+        const randomnessX = Math.pow(Math.random(), parametrs.pandomnessPower) * (Math.random() < 0.5 ? 0.3 : -0.3)
+        const randomnessY = Math.pow(Math.random(), parametrs.pandomnessPower) * (Math.random() < 0.5 ? 1 : -1)
+        const randomnessZ = Math.pow(Math.random(), parametrs.pandomnessPower) * (Math.random() < 0.5 ? 1 : -1)
+
+        randomness[i3] = randomnessX
+        randomness[i3 + 1] = randomnessY
+        randomness[i3 + 2] = randomnessZ
+
+        const mixedColor = colorInside.clone()
+        mixedColor.lerp(colorOutside, radius / parametrs.radius)
+
+        colors[i3] = mixedColor.r
+        colors[i3 + 1] = mixedColor.g
+        colors[i3 + 2] = mixedColor.b
+
+        scales[i] = Math.random()
+    }
+
+    geometry.setAttribute('position',
+        new THREE.BufferAttribute(positions, 3))
+
+    geometry.setAttribute('color',
+        new THREE.BufferAttribute(colors, 3))
+
+    geometry.setAttribute('aScale',
+        new THREE.BufferAttribute(scales, 1))
+
+    geometry.setAttribute('aRandomness',
+        new THREE.BufferAttribute(randomness, 3))
+
+
+
+    material = new THREE.ShaderMaterial({
+        depthWrite: false,
+        blending: THREE.AdditiveBlending,
+        vertexColors: true,
+        vertexShader: vertex,
+        fragmentShader: fragment,
+
+        uniforms: {
+        
+                uSize: {value: 15 * renderer.getPixelRatio()},
+                uTime: { value: 0}
+
+            
+        }
+    })
+    points = new THREE.Points(geometry, material)
+    scene.add(points)
 
 }
+
+
+gui.add(parametrs, 'count').min(100).max(100000).step(100).onFinishChange(generatyGalaxy)
+gui.add(parametrs, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(generatyGalaxy)
+gui.add(parametrs, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generatyGalaxy)
+gui.add(parametrs, 'branches').min(2).max(20).step(1).onFinishChange(generatyGalaxy)
+gui.add(parametrs, 'spin').min(-5).max(5).step(0.001).onFinishChange(generatyGalaxy)
+gui.add(parametrs, 'randomness').min(0).max(2).step(0.001).onFinishChange(generatyGalaxy)
+gui.add(parametrs, 'pandomnessPower').min(1).max(10).step(0.001).onFinishChange(generatyGalaxy)
+gui.addColor(parametrs, 'insideColor').onFinishChange(generatyGalaxy)
+gui.addColor(parametrs, 'outsideColor').onFinishChange(generatyGalaxy)
+
+
+const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000000);
+
+const renderer = new THREE.WebGLRenderer(
+    {
+        antialias: true,
+    }
+);
+
+document.body.appendChild(renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
+// renderer.shadowMap.enabled = true
+
+
+
+
+
+camera.position.z = 2;
+camera.position.x = 0;
+camera.position.y = 2.5;
+
+controls.update();
+
+// собственная геоиетрия
+
+
+scene.background = new THREE.Color('black')
+
+
+
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+}
+
+
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+
+const clock = new THREE.Clock()
+window.addEventListener('resize', onWindowResize, true)
+
+generatyGalaxy()
+
+function animate() {
+
+    const elepsedTime = clock.getElapsedTime()
+    material.uniforms.uTime.value = elepsedTime; 
+
+    renderer.render(scene, camera);
+    controls.update();
+    renderer.setSize(innerWidth, innerHeight);
+
+}
+
+renderer.setAnimationLoop(animate);
--- shaders/fragment.js
+++ shaders/fragment.js
@@ -4,18 +4,20 @@ export default /*glsl*/`
 
 varying vec2 vUv;
 varying float vElevation;
+varying vec3 vColor;
 
-uniform vec3 udDephColor;
-uniform vec3 uSurfaceColor;
-uniform float uColorOffset;
-uniform float uColorMultiplier;
 
 
         void main() {
-    float mixStrength = (vElevation + uColorOffset) * uColorMultiplier;
-    vec3 color = mix(udDephColor, uSurfaceColor, mixStrength);
-    gl_FragColor = vec4(color, 1.0);
+            float streength = distance(gl_PointCoord, vec2(0.5));
+            // streength = 1.0 - step(0.5, streength); // нарисовали круглый диск
+            streength *= 2.0;
+            streength = 1.0 - streength;
+            streength = pow(streength, 1.0);
 
+        vec3 color = mix(vec3(0.0), vColor, streength);
+
+        gl_FragColor = vec4(vec3(color), 1.0);
         }
 
 `;
