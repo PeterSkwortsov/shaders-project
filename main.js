cee29f8eb5c826907e02a341cf95c9bf3bdeb0cb
Peter <skwortsow.petia@yandex.ru>
2025-03-11 16:41:15 +0300

    Скручивание головы

--- main.js
+++ main.js
@@ -1,184 +1,239 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js'
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js'
-import vertex from './shaders/vertex';
-import fragment from './shaders/fragment'
 
-const scene = new THREE.Scene();
+
+const scene = new THREE.Scene()
+
+
 const gui = new GUI()
+const global = {}
 
-
-const asperctRatio = innerWidth / innerHeight
+const cubeTextureLoader = new THREE.CubeTextureLoader()
+const rgbeLoader = new RGBELoader()
 const textureLoader = new THREE.TextureLoader()
-
-// галактика
-
-const parametrs = {
-    count: 10000,
-    size: 0.02,
-    radius: 5,
-    branches: 3,
-    spin: 1,
-    randomness: 0.2,
-    pandomnessPower: 3,
-    insideColor: '#ff6030',
-    outsideColor: '#1b3984'
-}
-
-let geometry, material, points = null
-
-const generatyGalaxy = () => {
-
-    if (points !== null) {
-        geometry.dispose()
-        material.dispose()
-        scene.remove(points)
-    }
-
-    geometry = new THREE.BufferGeometry()
-    const positions = new Float32Array(parametrs.count * 3)
-
-    const colors = new Float32Array(parametrs.count * 3)
-    const scales = new Float32Array(parametrs.count * 1)
-    const randomness = new Float32Array(parametrs.count * 3)
+const gltfLoader = new GLTFLoader()
 
 
-    const colorInside = new THREE.Color(parametrs.insideColor)
-    const colorOutside = new THREE.Color(parametrs.outsideColor)
-    colorInside.lerp(colorOutside, 0.5)
+rgbeLoader.load('my-hdri-cart2.hdr', (environmentMap) => {
+    environmentMap.mapping = THREE.EquirectangularReflectionMapping
+    environmentMap.colorSpace = THREE.SRGBColorSpace
+
+    scene.background = environmentMap
 
 
-    for (let i = 0; i < parametrs.count; i++) {
+})
 
-        const radius = Math.random() * parametrs.radius
-        const spinAngle = radius * parametrs.spin
-        const branchesAngle = (i % parametrs.branches) / parametrs.branches * Math.PI * 2
-
-      
-
-        const i3 = i * 3
-        positions[i3] = Math.cos(branchesAngle + spinAngle) * radius;
-        positions[i3 + 1] = 0;
-        positions[i3 + 2] = Math.sin(branchesAngle + spinAngle) * radius;
+const updateAllMaterial = () => {
+    scene.traverse((child) => {
+        if (child.isMesh && child.material.isMeshStandardMaterial) {
+            child.material.envMapIntensity = 1
+            child.material.needsUpdate = true
+            child.castShadow = true
+            child.receiveShadow = true
+            child.scale.set(1, 1, 1)
 
 
-        const randomnessX = Math.pow(Math.random(), parametrs.pandomnessPower) * (Math.random() < 0.5 ? 0.3 : -0.3)
-        const randomnessY = Math.pow(Math.random(), parametrs.pandomnessPower) * (Math.random() < 0.5 ? 1 : -1)
-        const randomnessZ = Math.pow(Math.random(), parametrs.pandomnessPower) * (Math.random() < 0.5 ? 1 : -1)
-
-        randomness[i3] = randomnessX
-        randomness[i3 + 1] = randomnessY
-        randomness[i3 + 2] = randomnessZ
-
-        const mixedColor = colorInside.clone()
-        mixedColor.lerp(colorOutside, radius / parametrs.radius)
-
-        colors[i3] = mixedColor.r
-        colors[i3 + 1] = mixedColor.g
-        colors[i3 + 2] = mixedColor.b
-
-        scales[i] = Math.random()
-    }
-
-    geometry.setAttribute('position',
-        new THREE.BufferAttribute(positions, 3))
-
-    geometry.setAttribute('color',
-        new THREE.BufferAttribute(colors, 3))
-
-    geometry.setAttribute('aScale',
-        new THREE.BufferAttribute(scales, 1))
-
-    geometry.setAttribute('aRandomness',
-        new THREE.BufferAttribute(randomness, 3))
-
-
-
-    material = new THREE.ShaderMaterial({
-        depthWrite: false,
-        blending: THREE.AdditiveBlending,
-        vertexColors: true,
-        vertexShader: vertex,
-        fragmentShader: fragment,
-
-        uniforms: {
-        
-                uSize: {value: 15 * renderer.getPixelRatio()},
-                uTime: { value: 0}
-
-            
         }
     })
-    points = new THREE.Points(geometry, material)
-    scene.add(points)
-
 }
 
-
-gui.add(parametrs, 'count').min(100).max(100000).step(100).onFinishChange(generatyGalaxy)
-gui.add(parametrs, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(generatyGalaxy)
-gui.add(parametrs, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generatyGalaxy)
-gui.add(parametrs, 'branches').min(2).max(20).step(1).onFinishChange(generatyGalaxy)
-gui.add(parametrs, 'spin').min(-5).max(5).step(0.001).onFinishChange(generatyGalaxy)
-gui.add(parametrs, 'randomness').min(0).max(2).step(0.001).onFinishChange(generatyGalaxy)
-gui.add(parametrs, 'pandomnessPower').min(1).max(10).step(0.001).onFinishChange(generatyGalaxy)
-gui.addColor(parametrs, 'insideColor').onFinishChange(generatyGalaxy)
-gui.addColor(parametrs, 'outsideColor').onFinishChange(generatyGalaxy)
-
-
-const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000000);
-
-const renderer = new THREE.WebGLRenderer(
+const cubeRendererTarget = new THREE.WebGLRenderTarget(256,
     {
-        antialias: true,
+        type: THREE.HalfFloatType
     }
-);
-
-document.body.appendChild(renderer.domElement);
-const controls = new OrbitControls(camera, renderer.domElement);
-// renderer.shadowMap.enabled = true
+)
 
 
+scene.environment = cubeRendererTarget.texture
+// scene.background = environmentMap
+// scene.environment = environmentMap
 
+const ambientLight = new THREE.AmbientLight(0xffffff, 1)
+ambientLight.visible = true
+scene.add(ambientLight)
 
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
+camera.position.set(0, 2.3, 10)
 
-camera.position.z = 2;
-camera.position.x = 0;
-camera.position.y = 2.5;
+const renderer = new THREE.WebGLRenderer({ antialias: true })
+renderer.toneMapping = THREE.ACESFilmicToneMapping
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
 
-controls.update();
-
-// собственная геоиетрия
-
-
-scene.background = new THREE.Color('black')
-
-
-
-
-function onWindowResize() {
+window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
     renderer.setSize(window.innerWidth, window.innerHeight)
-}
+})
 
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
 
+renderer.toneMapping = THREE.ACESFilmicToneMapping
+renderer.toneMappingExposure = 2
+renderer.shadowMap.enabled = true
 renderer.shadowMap.type = THREE.PCFSoftShadowMap
 
-const clock = new THREE.Clock()
-window.addEventListener('resize', onWindowResize, true)
 
-generatyGalaxy()
 
-function animate() {
+const mapTexture = textureLoader.load('lpshead/color.jpg')
+mapTexture.colorSpace = THREE.SRGBColorSpace
+const normalTexture = textureLoader.load('lpshead/normal.jpg')
 
-    const elepsedTime = clock.getElapsedTime()
-    material.uniforms.uTime.value = elepsedTime; 
+// Material
+const material = new THREE.MeshStandardMaterial({
+    map: mapTexture,
+    normalMap: normalTexture
+})
 
-    renderer.render(scene, camera);
-    controls.update();
-    renderer.setSize(innerWidth, innerHeight);
+const depthMaterial = new THREE.MeshDepthMaterial({
+    depthPacking: THREE.RGBADepthPacking
+})
+
+const customUniforms = {
+    uTime: { value: 0 }
+}
+
+
+
+material.onBeforeCompile = (shader) => {
+
+    shader.uniforms.uTime = customUniforms.uTime
+
+    shader.vertexShader = shader.vertexShader.replace(
+        '#include <common>',
+        `
+            #include <common>
+
+            uniform float uTime;
+
+            mat2 get2dRotateMatrix(float _angle)
+            {
+                return mat2(cos(_angle), - sin(_angle), sin(_angle), cos(_angle));
+            }
+        `
+    )
+    shader.vertexShader = shader.vertexShader.replace(
+        '#include <beginnormal_vertex>',
+        `
+            #include <beginnormal_vertex>
+
+            float angle = (sin(position.y + uTime * 0.5)) * 0.9;
+            mat2 rotateMatrix = get2dRotateMatrix(angle);
+
+            objectNormal.xz = rotateMatrix * objectNormal.xz;
+
+
+        `
+    )
+    shader.vertexShader = shader.vertexShader.replace(
+        '#include <begin_vertex>',
+        `
+            #include <begin_vertex>
+            
+            angle = (position.y + uTime * 0.5) * 0.9;
+
+            transformed.xz = rotateMatrix * transformed.xz;
+        `
+    )
 
 }
 
-renderer.setAnimationLoop(animate);
+depthMaterial.onBeforeCompile = (shader) => {
+    shader.uniforms.uTime = customUniforms.uTime
+
+    shader.vertexShader = shader.vertexShader.replace(
+        '#include <common>',
+        `
+            #include <common>
+
+            uniform float uTime;
+
+            mat2 get2dRotateMatrix(float _angle)
+            {
+                return mat2(cos(_angle), - sin(_angle), sin(_angle), cos(_angle));
+            }
+        `
+    )
+    shader.vertexShader = shader.vertexShader.replace(
+        '#include <begin_vertex>',
+        `
+            #include <begin_vertex>
+
+            float angle = (sin(position.y + uTime * 0.5)) * 0.4;
+            mat2 rotateMatrix = get2dRotateMatrix(angle); 
+            
+            transformed.xz = rotateMatrix * transformed.xz;
+        `
+    )
+
+}
+
+
+
+global.envMapIntensity = scene.environmentIntensity
+
+
+
+scene.environmentIntensity = 1
+
+gltfLoader.load('lpshead/LeePerrySmith.glb', (gltf) => {
+    const mesh = gltf.scene.children[0]
+    // mesh.rotation.y = Math.PI * 0.5
+    mesh.material = material
+    mesh.customDepthMaterial = depthMaterial
+    scene.add(mesh)
+
+    // Update materials
+    updateAllMaterial()
+})
+
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(15, 15, 15),
+    new THREE.MeshStandardMaterial()
+)
+plane.rotation.y = Math.PI
+plane.position.y = - 5
+plane.position.z = 5
+scene.add(plane)
+
+
+/**
+ * Lights
+ */
+const directionalLight = new THREE.DirectionalLight('#ffffff', 3)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.normalBias = 0.05
+directionalLight.position.set(0.25, 2, - 2.25)
+scene.add(directionalLight)
+
+
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFShadowMap
+renderer.toneMapping = THREE.ACESFilmicToneMapping
+renderer.toneMappingExposure = 1
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+
+
+
+
+const clock = new THREE.Clock()
+function animate() {
+    const elapsedTime = clock.getElapsedTime()
+
+    customUniforms.uTime.value = elapsedTime
+
+    controls.update()
+    renderer.render(scene, camera)
+    requestAnimationFrame(animate)
+
+}
+
+
+animate()
